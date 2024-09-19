e592312a0c1836a311a67c5e4fcfb27d019a1a8b
Tarun Sai Srinivas <tarunsaisrinivas7@gmail.com>
2024-09-19 12:50:14 +0530

    hero updated

--- src/pages/HeroSection.jsx
+++ src/pages/HeroSection.jsx
@@ -54,7 +54,7 @@ const Banner = () => {
           {/* Register Now Button */}
           <a
             // href="https://docs.google.com/forms/d/e/1FAIpQLSdVyjJdFcq6N0cF77GUCD1Uag61aYVizyVm-Y0PMbTMCQmbig/formResponse"
-            href='https://scc-hackoverflow.vercel.app' target='_blank'
+            href='https://forms.gle/XuPZN1aBkxoofAqG7' target='_blank'
             className="px-10 py-3 bg-white text-gray-900 font-bold rounded-full shadow-lg hover:bg-gray-900 hover:text-white hover:border-white hover:border-2 transition-colors duration-300"
           >
             Register Now
